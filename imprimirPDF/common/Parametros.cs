3e04c698cf68eb1a083425f889da9fa115f13b44
William Gomez <wgomez@moticacioncristiana.org>
2022-06-06 12:34:06 -0600

    lol

--- imprimirPDF/common/Parametros.cs
+++ imprimirPDF/common/Parametros.cs
@@ -7,6 +7,6 @@ namespace imprimirPDF.common
 {
     public class Parametros
     {
-        public const string CadenaConnexion = "";
+        public const string CadenaConnexion = "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.0.12)(PORT=1521)) (CONNECT_DATA=(SERVICE_NAME=MMC))); User Id=ADMINISTRADOR;Password=1nfabc123;";
     }
 }
